686911dd5b31826dfa61dce322b763aef406ce8d
SIDHANIE <102436523+sidhaniedotcom@users.noreply.github.com>
2022-04-11 05:03:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1675749539:AAH46KNMRschhJDMuI5dclpp9FnB0znEaK4')
+const bot = new Telegraf('5093811483:AAFSlfvyolCLYujDl_nhdVuChVwckOFSACs')
 const axios = require('axios')
 
 /*********[ Apikey ]*********/
